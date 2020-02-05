56e34390b8e7ecd602c96d219f9d7c232227fd97
Wilson Acosta <sayhi@wilsonacosta.us>
2020-02-05 06:06:25 -0600

    Adjusted File Structure, Modified .htaccess, fixed navigation bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,29 @@
-
 //closes and opens mobile navigation
-$('#navCheckBox').click(function() {
-    var elementBody = $('body, #navLinks');
-    if (elementBody.css('margin-left') == '0px'){
-        elementBody.animate({ marginLeft : '+=300px'}, 450 );
+$('.navLinks, #navCheckBox').click(function() {
+    var elementBody = $('#navLinks');
+    if (elementBody.css('left') != '0px'){
+        elementBody.animate({ left : '+=300px'}, 450 );
     } else {
-        elementBody.animate({ marginLeft : '-=300px'}, 450 );
+        $("#menuToggle input:checked").prop("checked", false);
+        elementBody.animate({ left : '-=300px'}, 450 );
     }
 }) 
-  
 
+//Resets the Navigation and Menu Icon on resize
+window.onresize = function() {
+    var elementBody = $('#navLinks');
+    // if (window.innerHeight >= 820) { /* ... */ }
+    if (window.innerWidth >= 992) {  
+        elementBody.css('left', 'auto');
+    } else {
+        if (elementBody.css('left') != '0px'){
+            $("#menuToggle input:checked").prop("checked", false);
+            elementBody.css('left', '-300px');
+        }
+    }
+}
+  
+//Slow scroll down for anchor tags
 const speed = 1000;
 $('a[href^="#"]')
     .not('.lp-pom-form .lp-pom-button')
@@ -18,4 +32,11 @@ $('a[href^="#"]')
     event.preventDefault();
     const href = $(this).attr('href').split('#');
     $('html, body').animate({ scrollTop: $(`#${href[1]}`).offset().top-100 }, speed);
+
+    //Removes the #anchor in from the URL
+    window.location.replace("#");
+    if (typeof window.history.replaceState == 'function') {
+    history.replaceState({}, '', window.location.href.slice(0, -1));
+    }
 });
+
